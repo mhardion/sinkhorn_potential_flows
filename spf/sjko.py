fix gradients

spf/sjko.py
@@ -2,6 +2,7 @@ import torch
 from collections.abc import Callable
 from .utils import clampedlog, euclidean_cost
 from .optimizers import EulerianOptimizer, LagrangianOptimizer
+
 class EulerianSPF:
     def __init__(self, X: torch.Tensor, potential: Callable[[torch.Tensor], torch.Tensor] | torch.Tensor,
                  eps:float, c: Callable[[torch.Tensor, torch.Tensor], torch.Tensor] | torch.Tensor = euclidean_cost):
@@ -18,7 +19,7 @@ class EulerianSPF:
         if callable(potential):
             self.potential_array = potential(X)
         else:
-            if potential.size() != torch.Size(X.size(0)):
+            if potential.size() != torch.Size([X.size(0)]):
                 raise RuntimeError("potential array size should be X.size(0).")
             self.potential_array = potential
         self.X = X
@@ -38,9 +39,10 @@ class EulerianSPF:
         k = 1
         d = optim_tol + torch.ones(1)
         while k < max_optim_steps and (d > optim_tol).any():
-            g_10, f_01 = sinkhorn_loop(
+            f_01, g_10 = sinkhorn_loop(
                 µ1,
                 µ0,
+                self.cost_matrix,
                 self.cost_matrix,
                 [self.eps]*max_sinkhorn_steps,
                 init=schrodinger_potentials[:2],
@@ -67,7 +69,8 @@ class EulerianSPF:
             if print_progress:
                 print(f'\r SJKO step {i+1}/{t.size(0)-1}...', end='')
             µ = self.SJKO_step(µt[-1,:], tau, f0=f, **kwargs)
-            f = self_transport(µ, self.cost_matrix, self.eps, init=f)
+            f = self_transport(µ, self.cost_matrix, self.eps, init=f, tol=kwargs.get('sinkhorn_tol', 1e-4),
+                               max_steps=kwargs.get('max_sinkhorn_steps', 10))
             if return_ft:
                 ft = torch.cat((ft, f[None,:]), dim=0)
             µt = torch.cat((µt, µ[None,:]), dim=0)
@@ -104,10 +107,11 @@ class LagrangianSPF:
         k = 1
         schrodinger_potentials = [f0]*3 if f0 is not None else None
         while k < max_optim_steps:
-            g_10, f_01 = sinkhorn_loop(
+            f_01, g_10 = sinkhorn_loop(
                 self.µ,
                 self.µ,
-                self.c(x1, x0),
+                self.c(x1, x0.detach()),
+                self.c(x0, x1.detach()),
                 [self.eps]*max_sinkhorn_steps,
                 init=schrodinger_potentials[:2],
                 tol=sinkhorn_tol,
@@ -115,14 +119,15 @@ class LagrangianSPF:
             f1 = self_transport(self.µ, self.c(x1, x1), self.eps, init=schrodinger_potentials[2], tol=sinkhorn_tol,
                                   max_steps=max_sinkhorn_steps)
             schrodinger_potentials = [g_10, f_01, f1]
-            loss = (f_01 - f1 + g_10 +2*tau*self.potential(x1)).mean()
+            loss = (f_01 - f1 + g_10 + 2*tau*self.potential(x1)).mean()
             grad = torch.autograd.grad(loss, x1)[0]
             x1 = self.optimizer.step(x1, grad)
             k+=1
         return x1.detach()
     
     def integrate(self, x0: torch.Tensor, t: torch.Tensor, print_progress: bool = False, **kwargs):
-        f = self_transport(x0, self.c(x0, x0), self.eps)
+        f = self_transport(x0, self.c(x0, x0), self.eps, tol=kwargs.get('sinkhorn_tol', 1e-4),
+                           max_steps=kwargs.get('max_sinkhorn_steps', 10))
         xt = x0[None,:]
         
         for i, tau in enumerate(torch.diff(t)):
@@ -138,35 +143,34 @@ class LagrangianSPF:
 def softmin(eps, c, f):
     return -eps * (f.view(1, -1) - c / eps).logsumexp(1).view(-1)
 
-def sinkhorn_loop(µ1, µ2, c_xy, eps_list, init=None, tol=1e-4):
-    c_yx = c_xy.T
-    logµ1 = clampedlog(µ1)
-    logµ2 = clampedlog(µ2)
+def sinkhorn_loop(µ1, µ2, c_xy, c_yx, eps_list, init=None, tol=1e-4):
+    log_µ1 = clampedlog(µ1)
+    log_µ2 = clampedlog(µ2)
     torch.autograd.set_grad_enabled(False)
     eps = eps_list[0]
     if init is None:
-        g_12 = softmin(eps, c_yx, logµ1)
-        f_21 = softmin(eps, c_xy, logµ2)
+        f12 = softmin(eps, c_xy, log_µ2)
+        g12 = softmin(eps, c_yx, log_µ1)
     else:
-        g_12, f_21 = init
+        g12, f12 = init
 
     for eps in eps_list:
-        ft_21 = softmin(eps, c_xy, logµ2 + g_12 / eps)
-        gt_12 = softmin(eps, c_yx, logµ1 + f_21 / eps)
-        if max(abs(ft_21 - f_21).max().item(),
-               abs(gt_12 - g_12).max().item()) < tol:
+        f12_ = softmin(eps, c_xy, log_µ2 + g12 / eps)
+        g12_ = softmin(eps, c_yx, log_µ1 + f12 / eps)
+        if max(abs(f12_ - f12).max().item(),
+               abs(g12_ - g12).max().item()) < tol:
             break
-        f_21, g_12 = 0.5 * (f_21 + ft_21), 0.5 * (g_12 + gt_12) 
+        f12, g12 = 0.5 * (f12 + f12_), 0.5 * (g12 + g12_) 
 
     torch.autograd.set_grad_enabled(True)
 
-    f_21, g_12 = (
-        softmin(eps, c_xy, (logµ2 + g_12 / eps).detach()),
-        softmin(eps, c_yx, (logµ1 + f_21 / eps).detach()),
+    f12, g12 = (
+        softmin(eps, c_xy, (log_µ2 + g12 / eps).detach()),
+        softmin(eps, c_yx, (log_µ1 + f12 / eps).detach()),
     )
-    return g_12, f_21
+    return f12, g12
 
-def self_transport(µ, c, eps, init = None, tol=1e-4, max_steps=5):
+def self_transport(µ, c, eps, init=None, tol=1e-4, max_steps=10):
     logµ = clampedlog(µ)
     torch.autograd.set_grad_enabled(False)
     if init is None:
@@ -176,11 +180,11 @@ def self_transport(µ, c, eps, init = None, tol=1e-4, max_steps=5):
     k = 0
     d = tol + 1
     while k < max_steps and d > tol:
-        k+=1
+        k += 1
         f_µ_ = f_µ.clone()
         f_µ = .5*(f_µ + softmin(eps, c, logµ + f_µ / eps))
         d = (f_µ-f_µ_).max().item()
     
     torch.autograd.set_grad_enabled(True)
-    f_µ = softmin(eps, c, logµ + f_µ / eps).detach()
+    f_µ = softmin(eps, c, (logµ + f_µ / eps).detach())
     return f_µ